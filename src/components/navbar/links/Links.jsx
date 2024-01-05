f5b6c52a152fe2b697d363b76917a71460533a65
vladjsp <vladjsp@gmail.com>
2024-01-05 22:41:30 +0200

    + active nav link style and admin navbar item logic

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
-
 import styles from './links.module.css';
+import NavLink from './navLink/NavLink';
 
 const links = [
   {
@@ -21,14 +20,24 @@ const links = [
   },
 ];
 
+//TODO - replace with real logic
+const session = true;
+const isAdmin = true;
+
 const Links = () => {
   return (
     <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: 'Login', path: '/login' }} />
+      )}
     </div>
   );
 };
