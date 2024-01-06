8c1abdf37bddeef9c09ef77ab922fd6c01b460ac
vladjsp <vladjsp@gmail.com>
2024-01-06 16:17:39 +0200

    + footer design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>vladjsp</div>
+      <div className={styles.text}>
+        Vlad creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
