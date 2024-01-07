2029cdbd669ea9bed736f4b201b6e47b62dc5abd
vladjsp <vladjsp@gmail.com>
2024-01-07 15:51:26 +0200

    * server actions example to save and delete post

--- src/libs/actions.js
+++ src/libs/actions.js
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache';
 import { Post } from './models';
-import { connectToDb } from './utils';
+import { connectToDb } from './connectToDb';
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
