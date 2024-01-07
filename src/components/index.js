f68687fc14e8a3ea68bcfb75b993c912aa21d74a
vladjsp <vladjsp@gmail.com>
2024-01-07 18:04:58 +0200

    * minor refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ import Footer from './footer/Footer';
 import PostCard from './postCard/postCard';
 import PostUser from './postUser/postUser';
 import RegisterForm from './registerForm/registerForm';
-import LoginForm from './loginForm/loginPage';
+import LoginForm from './loginForm/loginForm';
 
 export { Navbar, Footer, PostCard, PostUser, RegisterForm, LoginForm };
